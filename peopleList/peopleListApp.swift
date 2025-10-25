dbbd97e92da69849da453bd0f3a217232a52b794
JoJaCo <contrerasjorge226@gmail.com>
2025-10-24 22:38:33 -0700

    Initial Commit

---
+++ peopleList/peopleListApp.swift
@@ -0,0 +1,17 @@
+//
+//  peopleListApp.swift
+//  peopleList
+//
+//  Created by Jorge Contreras on 10/24/25.
+//
+
+import SwiftUI
+
+@main
+struct peopleListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ peopleListTests/peopleListTests.swift
@@ -0,0 +1,17 @@
+//
+//  peopleListTests.swift
+//  peopleListTests
+//
+//  Created by Jorge Contreras on 10/24/25.
+//
+
+import Testing
+@testable import peopleList
+
+struct peopleListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
